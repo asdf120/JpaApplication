7072412991554483bb873584c1184220d1354661
Kim yonggwan <sssw0101@naver.com>
2022-03-14 16:59:42 +0900

    회원 레포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    // jpql 사용, 테이블이 아닌 entity를 통해 찾음
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)   // 쿼리, 반환하는 클래스
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name).getResultList();
+    }
+}
