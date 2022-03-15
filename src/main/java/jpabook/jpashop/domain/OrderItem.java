5de5ff73a80be8417218d1a0259dd50c39cdc0fb
Kim yonggwan <sssw0101@naver.com>
2022-03-15 12:53:49 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,28 @@ public class OrderItem {
     
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //생성 메소드
+    //할인 정책때문에 주문 당시 금액을 따로 받아줘야함
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);    //재고 차감
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
